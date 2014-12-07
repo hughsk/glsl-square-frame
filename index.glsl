09af9f0efddc53f6792033eff42d165fc13cdfc2
Yoshua Wuyts <yoshuawuyts@gmail.com>
2014-12-07 17:47:19 +0100

    screensize: fix uScreensize -> screenSize

--- index.glsl
+++ index.glsl
@@ -1,11 +1,11 @@
 vec2 squareFrame(vec2 screenSize) {
-  vec2 position = 2.0 * (gl_FragCoord.xy / uScreenSize.xy) - 1.0;
+  vec2 position = 2.0 * (gl_FragCoord.xy / screenSize.xy) - 1.0;
   position.x *= screenSize.x / screenSize.y;
   return position;
 }
 
 vec2 squareFrame(vec2 screenSize, vec2 coord) {
-  vec2 position = 2.0 * (coord.xy / uScreenSize.xy) - 1.0;
+  vec2 position = 2.0 * (coord.xy / screenSize.xy) - 1.0;
   position.x *= screenSize.x / screenSize.y;
   return position;
 }
